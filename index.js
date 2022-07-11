663714e174091a466d3c637f64274d4af16ed973
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-11 16:44:41 +0300

    rename middleware folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const connect = require('./database/connect')
-const auth = require('./middlewares/auth.middleware')
+const auth = require('./middleware/auth.middleware')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
