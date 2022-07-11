9c7fcc22507fa8bca7340893958256a1dfa0de5f
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-11 16:10:21 +0300

    finish auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,12 +1,22 @@
+const Admin = require("../models/Admin")
+
 function auth(req, res, next) {
   const login = req.body.login
   const password = req.body.password
 
   if (!login || !password) {
-    return res.json({
+    return res.status(400).json({
       message: "Properties 'login' and 'password' are required"
     })
   }
 
-  const candidate = await 
+  const candidate = await Admin.findOne({ login, password })
+
+  if (!candidate) {
+    return res.status(401).json({
+      message: "Authentication is required"
+    })
+  }
+
+  return res.json({ admin: candidate })
 }
